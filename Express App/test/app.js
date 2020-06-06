4bab7a5e55f9a14729449e8aa4f2c5547ca0a9c2
Jacob Krag <jacob.krag@outlook.dk>
2020-06-06 03:47:12 +0200

    Update

---
+++ Express App/test/app.js
@@ -0,0 +1,45 @@
+const bodyParser = require("body-parser")
+var express = require('express')
+var app = express()
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function (req, res) {
+    
+    //// Ends the response process. //// 
+    // res.end('No data send back sorry!')
+    
+
+    //// Send a response of various types. ////
+    // res.send({ some: 'json'})
+    // res.send('<p>some html</p>')
+    // res.status(404).send('Sorry, we cannot find that!')
+    // res.status(500).send({error: 'something blew up'})
+
+    //// Send a file as an octet stream. ////
+    res.sendFile(`${__dirname}/index.html`)
+
+    //// Render a view template. ////
+    // res.render('index', {someMessage: message});
+
+    //// Redirect a request ////
+    // res.redirect('/foo/bar')
+    // res.redirect('http://example.com')
+    // res.redirect(301, 'http://example.com')
+    // res.redirect('../login')
+
+    //// Send a JSON response. ////
+    // res.json({ user: 'tobi' })
+    // res.status(500).json({ error: 'message' })
+})
+
+app.post("/", function (req, res) {
+    const a = req.body.firstNumber;
+    const b = req.body.secondNumber;
+    let result = Number(a) + Number(b);
+    res.send(`The result is: ${result}`)
+})
+
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
+})
--- README.md
+++ README.md
@@ -1,22 +1,3 @@
-<style>
-    body {
-        color: white;
-    }
-    h1, h3 {
-         /* offset-x | offset-y | blur-radius | color */
-        box-shadow: 4px 4px 15px black;
-         /* top | right | bottom | left */
-        padding: 5px 5px 5px 5px;
-        font-weight: bold;
-    }
-
-    h2 {
-        border-left: 5px solid grey;
-        padding-left: 10px;
-        color: #4EDFB0;
-    }
-</style>
-
 # Node.js
 ## This is the README file
 Nothing here so far 😄
