7cfb7e49433200a37f4861ea327e51325e7f20ef
WhiteIronWolf <54797883+WhiteIronWolf@users.noreply.github.com>
2020-08-18 04:30:14 +0200

    Refresh

--- test/Path/app.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-console.log(path.join(__dirname, '..'));
-
-console.log(path.join(__dirname, '../..'));
-
-// console.log(path.join(__dirname, '../public'));
-
-console.log(__filename);
